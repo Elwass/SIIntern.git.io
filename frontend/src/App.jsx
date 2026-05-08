a145944b620745d16c526a3dfa06e20c920c1aef
SIC_04_ELWAS <107783540+Elwass@users.noreply.github.com>
2026-05-08 11:45:34 +0700

    Implement functional auth flows

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './components/auth/ProtectedRoute'
+import PublicOnlyRoute from './components/auth/PublicOnlyRoute'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import ResetPasswordPage from './pages/ResetPasswordPage'
 import AdminDashboard from './pages/AdminDashboard'
 import MentorDashboard from './pages/MentorDashboard'
 import StudentDashboard from './pages/StudentDashboard'
@@ -10,11 +14,17 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/mentor" element={<MentorDashboard />} />
-      <Route path="/student" element={<StudentDashboard />} />
-      <Route path="/report" element={<ReportPage />} />
+      <Route element={<PublicOnlyRoute />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/mentor" element={<MentorDashboard />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/report" element={<ReportPage />} />
+      </Route>
     </Routes>
   )
 }
